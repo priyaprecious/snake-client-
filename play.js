1a5e66e09bf1dbc07a572e1b8d6b08a8235e1c87
priyaprecious <priyanka.datti@gmail.com>
2021-11-06 05:34:02 +0000

    updated play.js

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",// IP address here,
+    port: 50541 // PORT number here,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("you are dead!")
+  })
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
